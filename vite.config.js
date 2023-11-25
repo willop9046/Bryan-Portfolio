67cd5967f05f5ff98f19efea0f18782350c87505
William Lin <uslwr314@gmail.com>
2023-11-25 06:29:19 -0800

    fix deployment setting

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Bryan-Portfolio",
+  base: "./Bryan-Portfolio",
   plugins: [react()],
 })
