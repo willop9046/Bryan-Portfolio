db2d70fb25b09d0c0ced1ffb348129804a7c04c2
William Lin <uslwr314@gmail.com>
2023-11-25 14:23:09 -0800

    fix deployment setting

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
 import Navbar from './Components/Navbar.jsx'
 import Footer from './Components/Footer.jsx'
@@ -14,7 +14,7 @@ import './main.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -23,7 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
 
   </React.StrictMode>,
 )
